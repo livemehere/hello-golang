70a8be9fd4d46f325bda8209f95431a6a82d4acf
livemehere <rhdxoals@gmail.com>
2026-09-13 23:17:06 +0900

    type assertion

--- 3.http-server/main.go
+++ 3.http-server/main.go
@@ -4,41 +4,21 @@ import (
 	"fmt"
 )
 
-type UserID int
-
-type Person interface {
-	SetName(name string)
-}
-
-type User struct {
-	Id   UserID
-	Name string
-	age  int
-}
-
-func (u User) hello() {
-	fmt.Println(u.Id, u.Name, u.age)
-}
-
-func (u *User) SetName(name string) {
-	u.Name = name
-}
-
-func DoSomthing(p Person) {
-	p.SetName("DODODO")
-}
-
 func main() {
-	me := User{
-		Id:   10,
-		Name: "Unknown",
-		age:  99,
+	var x any = 19
+
+	s, ok := x.(string)
+
+	if ok {
+		fmt.Println(s)
+	} else {
+		fmt.Println("failed!", ok)
 	}
 
-	var n UserID = 88
-	me.Id = n
-
-	//me.SetName("Kong")
-	DoSomthing(&me)
-	me.hello()
+	switch v := x.(type) {
+	case string:
+		fmt.Println("string:", v)
+	case int:
+		fmt.Println("int:", v)
+	}
 }
