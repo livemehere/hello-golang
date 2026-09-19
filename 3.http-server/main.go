e8f1d05781c1e4f040dc7a2f1fde7db64c96ecbf
livemehere <rhdxoals@gmail.com>
2026-09-19 13:10:46 +0900

    struct imbed

--- 3.http-server/main.go
+++ 3.http-server/main.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
+type Animal struct {
+	Name string
+}
+
+type Dog struct {
+	Animal
+	Breed string
+}
+
 func main() {
-	composed := "한"
-	decomposed := "\u1112\u1161\u11AB"
+	d := Dog{
+		Animal: Animal{Name: "kong"},
+		Breed:  "corgi",
+	}
 
-	fmt.Println(composed)
-	fmt.Println(decomposed)
-
-	fmt.Println(composed == decomposed) // false
+	fmt.Println(d)
 }
