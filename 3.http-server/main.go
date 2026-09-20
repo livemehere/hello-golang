f607a14403c0fdc60280b260423406cc04a03eba
livemehere <rhdxoals@gmail.com>
2026-09-20 11:55:45 +0900

    select

--- 3.http-server/main.go
+++ 3.http-server/main.go
@@ -4,24 +4,29 @@ import (
 	"fmt"
 )
 
+func fibonacci(ch, quit chan int) {
+	x, y := 0, 1
+
+	for {
+		select {
+		case ch <- x:
+			x, y = y, x+y
+		case <-quit:
+			return
+		}
+	}
+}
+
 func main() {
 	ch := make(chan int)
+	quit := make(chan int)
 
 	go func() {
-		ch <- 1
-		ch <- 2
-		ch <- 3
-
-		close(ch)
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-ch)
+		}
+		quit <- 0
 	}()
 
-	read := func() {
-		v, ok := <-ch
-		fmt.Println(v, ok)
-	}
-
-	read()
-	read()
-	read()
-	read()
+	fibonacci(ch, quit)
 }
